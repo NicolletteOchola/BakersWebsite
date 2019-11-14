63b3e98f76ad6fdfd2f51cb8c71e7f1710b1f96b
Nicole Ocholla <nicoleochola@gmail.com>
2019-11-14 14:33:17 +0300

    Added js to accept input.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var price, 1kgprice, 2kgprice;
+let total = 0;
+function GetOrder(flavour, size, baker, total) {
+  this.flavour = flavour;
+  this.flavour = size;
+  this.flavour = baker;
+  this.total = total;
+}
+
+$(document).ready(function ()) {}
